3618148d96fdaf0ecb054da753326c39a8167cbd
Marco Stagni <mrc.stagni@gmail.com>
2018-03-07 23:34:26 +0000

    changed stuff

--- src/P/ball.js
+++ src/P/ball.js
@@ -5,29 +5,29 @@ class Ball extends Particle {
     constructor({ radius, color, mass, charge, gradient} = {}) {
 
        super({mass, charge});
-       
+
        this.radius = radius === undefined ? 20 : radius;
        this.color = color === undefined ? '#0000ff' : color;
-       this.gradient = gradient === undefined ? false : gradient;   
-   }
+       this.gradient = gradient === undefined ? false : gradient;
+    }
 
-   draw(context) {
+    draw(context) {
 
-       const {x, y} = this.position; 
+        const {x, y} = this.position;
 
-       if (this.gradient){
+        if (this.gradient){
            let grad = context.createRadialGradient(x, y, 0, x, y, this.radius);
            grad.addColorStop(0, '#ffffff');
            grad.addColorStop(1, this.color);
            context.fillStyle = grad;
-       }else{
+        } else{
            context.fillStyle = this.color;
-       }
-       context.beginPath();
-       context.arc(x, y, this.radius, 0, 2*Math.PI, true);
-       context.closePath();
-       context.fill();
-   }
+        }
+        context.beginPath();
+        context.arc(x, y, this.radius, 0, 2*Math.PI, true);
+        context.closePath();
+        context.fill();
+    }
 }
 
 export default Ball;
