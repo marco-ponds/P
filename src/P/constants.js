fb14f4dc5d51540c5a1fc40002a42f4fc9412986
Marco Stagni <mrc.stagni@gmail.com>
2018-03-18 19:54:30 +0000

    example with balloon updated

--- src/P/constants.js
+++ src/P/constants.js
@@ -5,3 +5,5 @@ export const GRAVITATIONAL_CONSTANT = 1;
 // time related constants
 export const TIME_FRACTION = 0.005;
 export const MINIMUM_TIME_DIFFERENCE = 0.2;
+
+export const AIR_DRAG = 0.005;
--- src/P/forces.js
+++ src/P/forces.js
@@ -1,5 +1,7 @@
 import Vector from './vector';
 import {
+    G,
+    AIR_DRAG,
     GRAVITATIONAL_CONSTANT
 } from './constants';
 
@@ -17,8 +19,33 @@ export default class Forces {
         return r.clone().scale(-GRAVITATIONAL_CONSTANT * m1 * m2 /(r.lengthSquared() * r.length()))
     }
 
+    static upthrust(rho, volume) {
+        return new Vector(0, -rho * volume * G);
+    }
+
+    static get DRAG_TYPES() {
+        return {
+            SPHERE: 1
+        };
+    }
+
+    static linearDrag(object) {
+        const k = 6 * Math.PI * AIR_DRAG * object.radius;
+        let force,
+            velMag = object.velocity.length();
+
+        if (velMag > 0) {
+            force = object.velocity.clone().scale(-k);
+        }else {
+            force = new Vector(0,0);
+        }
+
+        return force;
+    }
+
     static drag(k, velocity) {
         let force;
+        
         const magnitude = velocity.length();
 
         if (magnitude > 0) {
